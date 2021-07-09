15130147c4dbe142541cb809cae64cd03a9a2cb0
Tigran Demerchyan <tigran.demerchyan@gmail.com>
2021-07-09 15:29:15 +0400

    change after review

--- src/com/sorting/SortAlgorithms.java
+++ src/com/sorting/SortAlgorithms.java
@@ -1,7 +1,6 @@
-package tiko;
+package com.sorting;
 
 public class SortAlgorithms {
-
 
     public static void insertionSort(int[] arr) {
         for (int i = 1; i < arr.length; i++) {
@@ -13,33 +12,26 @@ public class SortAlgorithms {
             }
             arr[j] = current;
         }
-
     }
-
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    swap(arr,j,j+1);
                 }
             }
         }
     }
-
-    static void swap(int[] arr, int i, int j) {
+   private static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
     private static int partition(int[] arr, int low, int high) {
-
         int pivot = arr[high];
         int i = (low - 1);
         for (int j = low; j <= high - 1; j++) {
-
             if (arr[j] < pivot) {
                 i++;
                 swap(arr, i, j);
@@ -52,16 +44,11 @@ public class SortAlgorithms {
     public static void quickSort(int[] arr) {
         quickSort(arr, 0, arr.length - 1);
     }
-
-    private static void quickSort(int[] arr, int start, int end) {
+    public static void quickSort(int[] arr, int start, int end) {
         if (start < end) {
-
             int pi = partition(arr, start, end);
-
             quickSort(arr, start, pi - 1);
             quickSort(arr, pi + 1, end);
         }
     }
-
-
 }
